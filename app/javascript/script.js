1f722c51ae7235001bc06300dd166effeb6f7d82
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-02-03 17:00:51 +0000

    chapter13-js

---
+++ app/javascript/script.js
@@ -0,0 +1,23 @@
+let title = "javascriptが使えました";
+alert(title);
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
+
+const opt = {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+
+$(document).on('turbolinks:load', function(){
+  let swiper = new Swiper('.swiper',opt);
+});
